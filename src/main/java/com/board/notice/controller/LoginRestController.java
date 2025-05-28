ed6d4f3ae95afd750743c4777121e5c81d8ee10c
SeungHeon <hun0018@naver.com>
2025-05-28 18:00:21 +0900

    5/28 JWT 코드 테스트 결과에 맞게 코드 수정

--- src/main/java/com/board/notice/controller/LoginRestController.java
+++ src/main/java/com/board/notice/controller/LoginRestController.java
@@ -1,6 +1,9 @@
 package com.board.notice.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,33 +14,58 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.board.notice.dto.request.LoginRequestDTO;
 import com.board.notice.dto.response.TokenResponseDTO;
+import com.board.notice.enums.Role;
 import com.board.notice.security.jwt.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequiredArgsConstructor
 public class LoginRestController {
 	private final JwtUtil jwtUtil;
 	private final AuthenticationManager authenticationManager;
-	
+
+//	로그인 버튼 누를 시
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequestDTO request) {
 		try {
-			// Security로 인증 
-			Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getId(), request.getPassword()));
+			// Security로 인증
+			Authentication authentication = authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(request.getId(), request.getPassword()));
 			// 인증 성공 시 사용자 정보 가져오기
 			UserDetails user = (UserDetails) authentication.getPrincipal();
 			// 토큰 생성(JWT)
 			String token = jwtUtil.createToken(user.getUsername(), jwtUtil.extractRole(user.getAuthorities()));
-			
-			return ResponseEntity.ok(new TokenResponseDTO(token));
-		} catch(AuthenticationException e) {
+			String refreshToken = jwtUtil.createRefreshToken(user.getUsername(),
+					jwtUtil.extractRole(user.getAuthorities()));
+
+			ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+					.httpOnly(true)
+					.path("/")
+					.maxAge(7 * 24 * 60 * 60)
+					.build();
+
+			return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE , cookie.toString()).body(new TokenResponseDTO(refreshToken));
+		} catch (AuthenticationException e) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 정보가 올바르지 않습니다. 다시 시도해주세요.");
 		}
 	}
 	
+//	accessToken 재생성 API
+	@PostMapping("/refresh-token")
+	public ResponseEntity<?> refreshToken(@CookieValue("refreshToken") String refreshToken) {
+		if(jwtUtil.isTokenValid(refreshToken)) {
+			String id = jwtUtil.getId(refreshToken);
+			Role role = jwtUtil.getRole(refreshToken);
+			String newAccessToken = jwtUtil.createToken(id, role);
+			return ResponseEntity.ok(new TokenResponseDTO(newAccessToken));
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("해당 RefreshToken은 유효하지 않은 토큰입니다.");
+		}
+	}
+
 }
--- src/main/java/com/board/notice/security/jwt/JwtUtil.java
+++ src/main/java/com/board/notice/security/jwt/JwtUtil.java
@@ -23,25 +23,30 @@ public class JwtUtil {
 	private final long accessTokenTime = 1000 * 60 * 30;
 	private final long refreshTokenTime = 1000 * 60 * 60 * 24 * 7;
 
+//	토큰 생성
 	public String createToken(String id, Role role) {
 
 		return Jwts.builder()
 				.setSubject(id)
 				.claim("Role", role)
+				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + accessTokenTime))
 				.signWith(SignatureAlgorithm.HS256, secretKey)
 				.compact();
 	}
 	
+//	ReFresh 토큰 생성
 	public String createRefreshToken(String id, Role role) {
 		
 		return Jwts.builder()
 				.setSubject(id)
+				.claim("Role", role)
+				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + refreshTokenTime))
 				.signWith(SignatureAlgorithm.HS256, secretKey)
 				.compact();
 	}
-	
+//	클라임 가져오기
 	public Claims getClaims(String token) {
 		
 		return Jwts.parserBuilder()
@@ -51,6 +56,7 @@ public class JwtUtil {
 				.getBody();
 	}
 	
+//	토큰 유효한지 여부 확인
 	public boolean isTokenValid(String token) {
 		try {
 			getClaims(token);
@@ -61,14 +67,17 @@ public class JwtUtil {
 		}
 	}
 	
+//	아이디 가져오기
 	public String getId(String token) {
 		return getClaims(token).getSubject();
 	}
 	
+//	롤(권한) 가져오기
 	public Role getRole(String token) {
 		return Role.valueOf(getClaims(token).get("Role", String.class));
 	}
 	
+//	권한 추출하기
 	public Role extractRole(Collection<? extends GrantedAuthority> authorities) {
 		for(GrantedAuthority authority : authorities) {
 			String rolename = authority.getAuthority();
